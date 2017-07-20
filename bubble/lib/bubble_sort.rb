7cf12291f1b1216d6ff642e398a0b05afe980af2
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 12:38:42 -0600

    Psuedocode and loop addition

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,10 +1,23 @@
 class BubbleSort
 
   def sort(array)
+    n = array.length
 
+    loop do
+      #["d", "b", "a", "c"]
+      #["b", "d", "a", "c"]
+      #["b", "a", "d", "c"]
+      #["a", "b", "d", "c"]
+      #["a", "b", "c", "d"]
+
+
+
+      swapped = false
+
+      (n - 1).times do |i|
+        
+    end
   end
-
-
 
 
 end
