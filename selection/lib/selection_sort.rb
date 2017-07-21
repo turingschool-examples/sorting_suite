307990264f84c9cf7c7c3c40633ae200b01fd7c4
snayrouz <samuel.nayrouz@gmail.com>
2017-07-21 12:30:13 -0600

    Selection sort pass

--- selection/lib/selection_sort.rb
+++ selection/lib/selection_sort.rb
@@ -1,10 +1,18 @@
-class SelectionSort
+class Selection
 
-
-
-
-
-
-
-
+  def sort(array)
+    new = array.length
+    for i in 0...new
+      min = i
+      for j in (i + 1)...new
+        if array[j]<array[min]
+          temp = array[j]
+          array[j] = array[min]
+          array[min] = temp
+        end
+      end
+    end
+    return array
+  end
+      
 end
--- selection/test/selection_sort_test.rb
+++ selection/test/selection_sort_test.rb
@@ -6,32 +6,32 @@ require './lib/selection_sort.rb'
 class SelectionSortTest < Minitest::Test
 
   def test_merge_exists
-    selection = SelectionSort.new
+    s = Selection.new
 
-    assert_instance_of MergeSort, selection
+    assert_instance_of Selection, s
   end
 
   def test_merge_can_sort
-    selection = SelectionSort.new
+    s = Selection.new
     array = ["d", "b", "a", "c"]
 
-    assert_equal ["a", "b", "c", "d"], selection.sort(array)
+    assert_equal ["a", "b", "c", "d"], s.sort(array)
   end
 
   def test_can_sort_numbers
-    selection = SelectionSort.new
+    s = Selection.new
     numbers = [4, 2, 1, 5, 3]
     actual = [1, 2, 3, 4, 5]
 
-    assert_equal actual, selection.sort(numbers)
+    assert_equal actual, s.sort(numbers)
   end
 
   def test_can_sort_neg_numbers
-    selection = SelectionSort.new
+    s = Selection.new
     numbers = [-4, -2, -1, -5, -3]
     actual = [-5, -4, -3, -2, -1]
 
-    assert_equal actual, selection.sort(numbers)
+    assert_equal actual, s.sort(numbers)
   end
 
 
