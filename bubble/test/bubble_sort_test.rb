b863968f5d0006f2e6de8a99d9fa028728a66a80
snayrouz <samuel.nayrouz@gmail.com>
2017-07-21 11:25:49 -0600

    Add bubble sort tests

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -12,11 +12,26 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort, b
   end
 
-  def test_can_sort
+  def test_can_sort_letters
     b = BubbleSort.new
     array = ["d", "b", "a", "c"]
 
     assert_equal ["a", "b", "c", "d"], b.sort(array)
   end
-  
+
+  def test_can_sort_numbers
+    b = BubbleSort.new
+    numbers = [4, 2, 1, 5, 3]
+
+    assert_equal numbers, b.sort(numbers)
+  end
+
+  def test_can_sort_neg_numbers
+    b = BubbleSort.new
+    numbers = [-4, -2, -1, -5, -3]
+
+    assert_equal numbers, b.sort(numbers)
+
+  end
+
 end
