08b6db23aafc9b8a10611c7eb27fe13d73244108
snayrouz <samuel.nayrouz@gmail.com>
2017-07-21 13:03:34 -0600

    Add benchmark module

---
+++ benchmark.rb
@@ -0,0 +1,26 @@
+require 'bubble_sort'
+require 'insertion_sort'
+require 'merge_sort'
+require 'selection_sort'
+
+# make to module
+
+class Benchmark
+
+  def timer(sort_name, array)
+
+  end
+
+
+
+  def time(sort_name, array)
+
+    puts "#{sort_name} took XXXX seconds"
+  end
+
+
+
+
+
+
+end
