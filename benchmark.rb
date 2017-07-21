1ef32ec15f4920a0548036cac90f556035873cc2
snayrouz <samuel.nayrouz@gmail.com>
2017-07-21 13:13:54 -0600

    Build benchmark mod

--- benchmark.rb
+++ benchmark.rb
@@ -3,24 +3,33 @@ require 'insertion_sort'
 require 'merge_sort'
 require 'selection_sort'
 
-# make to module
+module SortingSuite
 
-class Benchmark
+  class Benchmark
 
-  def timer(sort_name, array)
+    def timer(sort_name, array)
+      start = Time.now
+
+      if sort_name == SortingSuite::BubbleSort
+        SortingSuite::BubbleSort.new.sort(array)
+      elsif sort_name == SortingSuite::InsertionSort
+        SortingSuite::InsertionSort.new.sort(array)
+      elsif sort_name == SortingSuite::MergeSort
+        SortingSuite::MergeSort.new.sort(array)
+      elsif sort_name == SortingSuite:SelectionSort
+        SortingSuite::SelectionSort.new.sort(array)
+      end
+      final = Time.new
+      return final - start
+    end
+
+    def time(sort_name, array)
+      puts "#{sort_name} took #{timer(sort_name, array)} seconds"
+    end
+
+
+
+
 
   end
-
-
-
-  def time(sort_name, array)
-
-    puts "#{sort_name} took XXXX seconds"
-  end
-
-
-
-
-
-
 end
