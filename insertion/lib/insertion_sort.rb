351efd205394bb5cbeae6c5fa7efcb61a7b30989
samuelssnider <sam_redins@yahoo.com>
2017-07-21 15:05:10 -0600

    work on is and tests

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,2 +1,21 @@
 class InsertionSort
+  def sort(in_set)
+    if in_set.length <= 1
+      result_set = in_set[0]
+    else
+      sorted_array = []
+      in_set.each_with_index do |element, outside|
+        if outside == 0
+          sorted_array << in_set[outside]
+        else
+          sorted_array.each_with_index do |sorted, inside|
+            if in_set[outside] > sorted[inside]
+              sorted_array.insert(inside, inset[outside]
+            end
+          end
+        end
+      end
+    end
+    result_set
+  end
 end
--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -11,4 +11,42 @@ class InsertionSortTest < Minitest::Test
     assert_instance_of InsertionSort, is
   end
 
+  def test_it_can_sort_nothing
+    is = InsertionSort.new
+
+    assert_nil is.sort([])
+  end
+
+  def test_it_can_sort_one_element
+    is = InsertionSort.new
+
+    assert_equal "a", is.sort(["a"])
+  end
+
+  def test_it_can_sort_multiple_elements_once_through
+    is = InsertionSort.new
+
+    assert_equal ["a", "b", "c", "d"], is.sort(["b", "a", "d", "c"])
+  end
+
+  def test_its_last_element_is_always_working
+    is = InsertionSort.new
+
+    assert_equal "d", is.sort(["b", "d", "c", "a"]).last
+    assert_equal "z", is.sort(["b", "z", "d", "c", "a", "r", "o", "x", "p", "m"]).last
+  end
+
+  def test_it_can_sort_more_mixed_elements
+    is = InserionSort.new
+
+    assert_equal ["a", "b", "c", "d"], is.sort(["b", "d", "c", "a"])
+  end
+
+  def test_it_can_sort_a_large_number_of_elements
+    is = BubbleSort.new
+
+    sorted = ["a", "b", "c", "d", "m", "o", "p", "r", "x", "z"]
+    assert_equal sorted, is.sort(["b", "z", "d", "c", "a", "r", "o", "x", "p", "m"])
+  end
+
 end
