33419edd146592440013ba80fe79e3153389f98e
Mike Heft <mikeheft@gmail.com>
2017-07-21 19:47:44 -0600

    it can sort mixed letters

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -20,4 +20,10 @@ class BubbleSortTest < Minitest::Test
     numbers = [3,6,2,8,1,5]
     assert_equal [1,2,3,5,6,8], sorter.sorted(numbers)
   end
+
+  def test_it_can_sort_up_and_downcase
+    sorter = BubbleSort.new
+    letters = ["d", "b", "a", "C"]
+    assert_equal ["C","a","b","d"], sorter.sorted(letters)
+  end
 end
