dc38a9a08aa4a32021af5a7df5862c0b0b293a6e
mimilettd <mimilettd@gmail.com>
2017-07-22 16:05:21 -0600

    pseudo-code mergesort

--- merge/lib/merge.rb
+++ merge/lib/merge.rb
@@ -1,5 +1,20 @@
 class MergeSort
   def sort(array)
-    array
+    # split the array into nth list until each list has only one element.
+    # [2, 0, 1, 3]
+    # => [[2, 0], [1, 3]]
+    # => [2]
+    # => [0]
+    # => [1]
+    # => [3]
+    # compare the two arrays on the left side first and swap them accordingly. Put them back into an array together.
+    # => [0, 2]
+    # compare the two arrays on the right side and swap them accordingly. Put them back into an array together.
+    # => [1 ,3]
+    # compare the left array with the right away, starting with the first index.
+    # => 0
+    # => 0, 1
+    # => 0, 1, 2
+    # => 0, 1, 2, 3
   end
 end
--- merge/test/merge_test.rb
+++ merge/test/merge_test.rb
@@ -6,14 +6,25 @@ require 'pry'
 class MergeSortTest < Minitest::Test
   def test_class_exist
     merge = MergeSort.new
+
     assert_instance_of MergeSort, merge
   end
 
   def test_it_can_take_array_as_argument
     merge = MergeSort.new
 
-    array = [2, 0, 1, 3]
+    array = [2, 0, 1, 3, 4, 5]
 
     assert_instance_of Array, merge.sort(array)
+  end
+
+  def test_it_can_sort_an_array_of_numbers
+    merge = MergeSort.new
+
+    array = [2, 0, 1, 3]
+
+    expected = [0, 1, 2, 3]
+
+    assert_equal expected, merge.sort(array)
   end
 end
