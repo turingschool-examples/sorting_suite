339c5a4051206611c41d0746da431f7aeeaaa6ec
mimilettd <mimilettd@gmail.com>
2017-07-21 20:53:01 -0600

    pseudo-code bubble sort

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -1,5 +1,11 @@
 class Bubble
   def sort(array)
-    array
+    # Determine the length of the array
+    # Loop through the array starting from the zero index.
+      # Determine whether the first element in the array is larger than the second element in the array
+        # If it is larger, swap.
+      # Determine wehther the second element in the array is larger than the third element in the array
+        # If it is larger, swap.
+      # Iterate through the array until the array is sorted lowest to highest. 
   end
 end
