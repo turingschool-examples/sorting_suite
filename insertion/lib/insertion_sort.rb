551525ab9d5a565fc43ca3f18d2c7ea9b392ee05
Becki Srofe <blf_23@yahoo.com>
2017-07-22 11:49:03 -0600

    Add working insertion sort class

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,2 +1,44 @@
 class InsertionSort
+
+  def sort(array)
+    sorted = []
+    sorted << array.shift
+    array.each do |num|
+      if num < sorted[0]
+        sorted.unshift(num)
+      else
+        sorted << num
+        bubble(sorted)
+      end
+    end
+    sorted
+  end
+
+  def bubble(array)
+    iterations = array.length - 1
+    iterations.times do 
+      iterate(array)
+    end
+    array
+  end
+
+  def iterate (array)
+    previous = array[0]
+    array.each_with_index do |num, index|
+      if index == 0
+        next
+      elsif num < previous
+        swap(array, index)
+      else
+        previous = array[index]
+      end
+    end
+  end
+
+  def swap(array, index)
+    new_index = index - 1
+    array[index], array[new_index] = array[new_index], array[index]
+    previous = array[index]
+  end
+
 end
--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -11,4 +11,10 @@ class InsertionSortTest < Minitest::Test
     assert_instance_of InsertionSort, sorter
   end
 
+  def test_it_can_sort_array
+    sorter = InsertionSort.new
+    array = [1,0,4,3,2]
+
+    assert_equal [0,1,2,3,4], sorter.sort([1,0,4,3,2])
+  end
 end
