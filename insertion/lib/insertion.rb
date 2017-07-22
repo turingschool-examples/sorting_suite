4b6538df320b6aa2c3eea7e59952513d8eff6753
mimilettd <mimilettd@gmail.com>
2017-07-22 12:06:57 -0600

    test it can sort an array of numbers

--- insertion/lib/insertion.rb
+++ insertion/lib/insertion.rb
@@ -9,5 +9,26 @@ class InsertionSort
       # If the unsorted element is smaller than the first element in the sorted array, insert it before the sorted element.
       # Else, compare it to the second sorted element. If it is smaller, insert it before the second sorted element. If it is larger, insert it after.
     # Repeat until the unsorted array is empty.
+    sorted = []
+    sorted << array[0]
+    array.shift
+    i = 0
+    current = array[0]
+    until array.empty?
+      if sorted[i] > current
+        sorted.insert(i, current)
+        array.shift
+        current = array[0]
+      else
+        i += 1
+        current = array[0]
+        if sorted[i].nil?
+          sorted.insert(i, current)
+          array.shift
+          # binding.pry
+        end
+      end
+    end
+    sorted
   end
 end
