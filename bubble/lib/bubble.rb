efdbaae7c78b74d8c6c44c9fed55043170d7000e
Becki Srofe <blf_23@yahoo.com>
2017-07-21 21:01:35 -0600

    Add two methods, neither work

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -1,2 +1,38 @@
 class Bubble
+
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  # def sorter
+  #   previous = @array[0]
+  #   @array.each_with_index do |num, index|
+  #     next if index == 0
+  #     if num < previous
+  #       new_index = index - 1
+  #       @array[index], @array[new_index] = @array[new_index], @array[index]
+  #     end
+  #     previous = num
+  #   end
+  #   @array
+  # end
+
+  def sorter
+    previous = @array[0]
+    @array.each_with_index do |num, index|
+      if index == 0
+        next
+      elsif num < previous
+        new_index = index - 1
+        @array[index], @array[new_index] = @array[new_index], @array[index]
+        previous = @array[index]#might be wrong
+      else
+        previous = @array[index]
+      end
+    end
+    @array
+  end
+
 end
--- bubble/test/bubble_test.rb
+++ bubble/test/bubble_test.rb
@@ -1,14 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'minitest/emoji'
-require './lib/sortingsuite'
+require './lib/bubble'
 
-class SortingsuiteTest < Minitest::Test
+class BubbleTest < Minitest::Test
 
   def test_it_exists
-    suite = Sortingsuite.new
+    bubble = Bubble.new([])
 
-    assert_instance_of Sortingsuite, suite
+    assert_instance_of Bubble, bubble
+  end
+
+  def test_it_has_an_array
+    bubble = Bubble.new([2,0,1])
+
+    assert_equal [0, 1, 2], bubble.array
   end
 
 end
