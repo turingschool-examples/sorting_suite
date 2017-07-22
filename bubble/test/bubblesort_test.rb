44575429ea44d742ca913960f562158824f40a04
Brandon Stewart <boveus@gmail.com>
2017-07-21 21:31:04 -0600

    Add number test to bubblesort.

--- bubble/test/bubblesort_test.rb
+++ bubble/test/bubblesort_test.rb
@@ -16,4 +16,15 @@ class BubbleSortTest < Minitest::Test
     assert_equal ["a", "b", "c", "d"], sorter.sort(letter_array2)
     assert_equal ["a", "b", "c", "d"], sorter.sort(letter_array3)
   end
+
+  def test_sort_using_numbers
+    sorter = BubbleSort.new
+    number_array = [3, 5, 1, 7, 10]
+    number_array2 = [5, 3, 1, 10, 7]
+    number_array3 = [31, 50, 1, 70, 100000]
+
+    assert_equal [1, 3, 5, 7, 10], sorter.sort(number_array)
+    assert_equal [1, 3, 5, 7, 10], sorter.sort(number_array2)
+    assert_equal [1, 31, 50, 70, 100000], sorter.sort(number_array3)
+  end
 end
