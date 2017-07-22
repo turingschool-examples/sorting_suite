69beefacb7cfd87f3df7de69dee481050de98a18
Becki Srofe <blf_23@yahoo.com>
2017-07-22 11:53:39 -0600

    Add more insertion tests

--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -24,4 +24,18 @@ class InsertionSortTest < Minitest::Test
 
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
   end
+
+  def test_it_can_sort_negative_numbers
+    sorter = InsertionSort.new
+    array = [3, -1, 2, 0, -2, 1]
+
+    assert_equal [-2, -1, 0, 1, 2, 3], sorter.sort([3, -1, 2, 0, -2, 1])
+  end
+
+  def test_it_can_sort_if_numbers_are_skipped
+    sorter = InsertionSort.new
+    array = [2, 5, 0, 1, 7, 4]
+
+    assert_equal [0, 1, 2, 4, 5, 7], sorter.sort([2, 5, 0, 1, 7, 4])
+  end
 end
