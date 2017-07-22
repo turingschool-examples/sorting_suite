64dd391bd70c241b2e79f41c86f7c7cac8af1b6e
mimilettd <mimilettd@gmail.com>
2017-07-22 09:31:43 -0600

    pseudo code insertion class

--- insertion/lib/insertion.rb
+++ insertion/lib/insertion.rb
@@ -1,5 +1,13 @@
 class InsertionSort
   def sort(array)
-    array
+    # Create an empty `sorted` array
+    # Pull the first element from the unsorted array and insert it into the sorted array.
+    # Pull the first unsorted element and compare it to the first element of the sorted array.
+      # If the unsorted element is smaller than the first element of the sorted array, insert it before the sorted element.
+      # Else, insert it after the sorted element.
+    # Pull the first unsorted element and compare it to the first element in the sorted array.
+      # If the unsorted element is smaller than the first element in the sorted array, insert it before the sorted element.
+      # Else, compare it to the second sorted element. If it is smaller, insert it before the second sorted element. If it is larger, insert it after.
+    # Repeat until the unsorted array is empty.
   end
 end
--- insertion/test/insertion_test.rb
+++ insertion/test/insertion_test.rb
@@ -16,4 +16,14 @@ class InsertionSortTest < Minitest::Test
 
     assert_instance_of Array, insertion.sort(array)
   end
+
+  def test_it_can_sort_an_array
+    insertion = InsertionSort.new
+
+    array = [2, 0, 1, 3, 4, 5]
+
+    expected = [0, 1, 2, 3, 4, 5]
+
+    assert_equal expected, insertion.sort(array)
+  end
 end
