3f38d75176fe232c0843860217c5918ea59fc8d3
Mike Heft <mikeheft@gmail.com>
2017-07-21 19:52:01 -0600

    refactor-extract methods

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,11 +2,19 @@ class BubbleSort
 
   def sorted(array)
     array.each_index do |first|
-      array.each_index do |second|
-        if array[first] < array[second]
-          array[first],array[second] = array[second],array[first]
-        end
-      end
+      second_array(array, first)
+    end
+  end
+
+  def second_array(array,first)
+    array.each_index do |second|
+      comparison(array,first,second)
+    end
+  end
+
+  def comparison(array,first,second)
+    if array[first] < array[second]
+      array[first],array[second] = array[second],array[first]
     end
   end
 
