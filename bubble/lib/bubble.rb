8e5f974018cd6126dc693e4a0be2c5ee29741b94
mimilettd <mimilettd@gmail.com>
2017-07-22 08:36:00 -0600

    test sort method can sort

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -1,11 +1,25 @@
 class Bubble
   def sort(array)
     # Determine the length of the array
-    # Loop through the array starting from the zero index.
-      # Determine whether the first element in the array is larger than the second element in the array
-        # If it is larger, swap.
-      # Determine wehther the second element in the array is larger than the third element in the array
-        # If it is larger, swap.
-      # Iterate through the array until the array is sorted lowest to highest. 
+    # [2, 0, 1, 3, 4, 5] => 6
+    # Take the first element and loop through the array comparing its value against the next element. If it is larger, swap and flip the value of `swap` to true.
+    # Repeat the loop but with n-1 less time.
+        # [2, 0, 1, 3, 4, 5] - swapped? true
+        # [0, 2, 1, 3, 4, 5] - swapped? true
+        # [0, 1, 2, 3, 4, 5] - swapped? false
+    # Break the loop when the value of swapped is false.
+    # Return the sorted array.
+    n = array.length
+    loop do
+      swapped = false
+      (n-1).times do |i|
+        if array[i] > array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    array
   end
 end
