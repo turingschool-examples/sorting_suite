a5a671de0ee3342a080151504680ca489491145a
Mike Heft <mikeheft@gmail.com>
2017-07-23 11:33:24 -0600

    refactor, move method

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -3,15 +3,24 @@ class InsertionSort
   def sorter(array)
     (1...array.length).each do |index|
       while index > 0
-        if array[index-1] > array[index]
-          temp = array[index]
-          array[index] = array[index-1]
-          array[index-1] = temp
-        end
+        comparison(array, index)
+        # if array[index-1] > array[index]
+        #   temp = array[index]
+        #   array[index] = array[index-1]
+        #   array[index-1] = temp
+        # end
         index -= 1
       end
     end
     return array
+  end
+
+  def comparison(array, index)
+    if array[index-1] > array[index]
+      temp = array[index]
+      array[index] = array[index-1]
+      array[index-1] = temp
+    end
   end
 
 end
