4621e57347ae26b4c22a5f8454f74f1002d2b2a5
Mike Heft <mikeheft@gmail.com>
2017-07-23 14:16:17 -0600

    merged

---
+++ merge/test/merge_sort_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/merge_sort'
+
+class MergeSortTest < Minitest::Test
+  attr_reader :sorter
+
+  def setup
+    @sorter = MergeSort.new
+  end
+
+  def test_its_a_thing
+    assert_instance_of MergeSort, sorter
+  end
+
+  def test_it_can_sort_letter
+    letters = ["d", "b", "e", "a", "c"]
+    assert_equal ["a","b","c","d","e"], sorter.sorted(letters)
+  end
+
+  def test_it_can_sort_numbers
+    numbers = [3,6,2,7,3,8]
+    expected = [2,3,3,6,7,8]
+    assert_equal expected, sorter.sorted(numbers)
+  end
+
+end
