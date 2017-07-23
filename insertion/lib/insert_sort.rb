189c97e9abd7786151665ade0f6fb7352afcc3c4
JunePaloma <kborkoski@gmail.com>
2017-07-22 21:20:40 -0600

    Add mostly working insert sort with tests

--- insertion/lib/insert_sort.rb
+++ insertion/lib/insert_sort.rb
@@ -1,17 +1,105 @@
 # At each array-position, it checks the value there against the largest value in the sorted list (which happens to be next to it, in the previous array-position checked). If larger, it leaves the element in place and moves to the next. If smaller, it finds the correct position within the sorted list, shifts all the larger values up to make a space, and inserts into that correct position.
 
 class InsertSort
-  attr_reader :sorted,
+  attr_reader
               :unsorted
 
   def initialize(arr)
-    @sorted = []
     @unsorted = arr
   end
 
-  def send_first_element_into_sorted
-    @sorted << @unsorted.shift
-  end
 
+  def sort
+    sorted = []
+    # index = 0
+    # final_index = @unsorted.length
+
+    sorted << @unsorted.shift
+
+      until @unsorted.empty?
+
+        @unsorted.each do |unsorted_element|
+
+          if unsorted_element < sorted.first
+            sorted.unshift(unsorted_element)
+              @unsorted-=[unsorted_element]
+
+          elsif unsorted_element > sorted.last
+            sorted << unsorted_element
+
+            @unsorted-=[unsorted_element]
+
+          elsif
+
+            sorted.each_with_index do |sorted_element, index|
+              if sorted_element > unsorted_element
+                  sorted.insert(index-1, unsorted_element)
+                @unsorted-=[unsorted_element]
+              elsif sorted_element < unsorted_element
+                sorted.insert(index+1, unsorted_element)
+                @unsorted-=[unsorted_element]
+            else
+              break
+              end
+            end
+          end
+        end
+      end
+      sorted
+    end
+
+
+
+
+    # if sorted.length == 1 && element > sorted[index]
+    #   sorted << element
+    #   @unsorted.shift
+    #
+    # elsif sorted.length == 1 && element < sorted[index]
+    #   sorted.unshift(element)
+    #     @unsorted.shift
+
+
+  # def run
+  #   until @sorted.empty?
+  #     send_first_element_into_sorted
+  #     initial_greater_than_less_than
+  #   end
+  # end
+  #
+  # def send_first_element_into_sorted
+  #
+  # end
+  #
+  # def initial_greater_than_less_than
+  #   if @unsorted[0] > @sorted[0]
+  #     is_element_greater_than
+  #   else
+  #     is_element_less_than
+  #   end
+  # end
+  #
+  # def is_element_greater_than
+  #   index = 0
+  #   if @sorted > @unsorted
+  #     @sorted.insert(index, @unsorted[0])
+  #   elsif @sorted[index] < @unsorted[0]
+  #     index+=1
+  #     is_element_greater_than
+  #   end
+  #   @sorted
+  # end
+  #
+  # def is_element_less_than
+  #   index = 0
+  #   if @sorted[index] < @unsorted[0]
+  #     @sorted.insert(index, @unsorted[0])
+  #   elsif @sorted[index] > @unsorted[0]
+  #     # @sorted.insert(index-1, @unsorted[index])
+  #     index+=1
+  #     is_element_less_than
+  #   end
+  #   @sorted
+  # end
 
 end
--- insertion/test/insert_sort_test.rb
+++ insertion/test/insert_sort_test.rb
@@ -6,13 +6,35 @@ require 'pry'
 
 class InsertSortTest < Minitest::Test
 
-  def test_it_takes_an_array
-    sorter = InsertSort.new([5,6,7])
-    assert_equal [5,6,7], sorter.unsorted
-  end
+  # def test_it_takes_an_array
+  #   sorter = InsertSort.new([5,6,7])
+  #   assert_equal [5,6,7], sorter.unsorted
+  # end
+  #
+  # def test_it_sends_values_to_sorted
+  #   sorter = InsertSort.new([4,3,2,1])
+  #   assert_equal [4], sorter.send_first_element_into_sorted
+  # end
+  #
+  # def test_it_compares_next_element
+  #   sorter = InsertSort.new([4,3,2,1])
+  #   sorter.send_first_element_into_sorted
+  #
+  #   assert_equal [3,4], sorter.initial_greater_than_less_than
+  # end
 
-  def test_it_sends_values_to_sorted
-        sorter = InsertSort.new([4,3,2,1])
-        assert_equal [4], sorter.send_first_element_into_sorted
+  def test_it_sorts
+    sorter = InsertSort.new([5, 8, 10, 13, 1, 56, 2])
+    assert_equal [1, 2, 5, 8, 10, 13, 56],sorter.sort
+
+    sorter = InsertSort.new(['h', 'e', 'b', 'g', 'd', 'f', 'a'])
+    assert_equal ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], sorter.sort
+
+    sorter = InsertSort.new([-1, 4, 7, -6, 3])
+    assert_equal [-6, -1, 3, 4, 7], sorter.sort
+
+    sorter = sorter = InsertSort.new(["c", "d", "b", "a"])
+    assert_equal ["a", "b", "c", "d"], sorter.sort
+
   end
 end
