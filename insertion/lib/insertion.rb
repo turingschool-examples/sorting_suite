6608f9b8c989107b1cfbcce2d36292f942ba62b0
Balmung421 <zcook421@gmail.com>
2017-07-23 13:36:53 -0600

    Added insertion sort test files

---
+++ insertion/lib/insertion.rb
@@ -0,0 +1,6 @@
+class Insertion
+
+
+
+
+end
---
+++ insertion/test/insertion_test.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/insertion'
+
+class InsertionSortTest < MiniTest::Test
+
+
+
+
+end
