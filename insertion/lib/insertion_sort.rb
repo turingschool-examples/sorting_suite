9c0f09a533a6de9c602f092ca9e0816a3f0f63c0
sidewinder2020 <sdwinder1@gmail.com>
2017-07-23 20:49:58 -0600

    random insertion code - as yet unfinished

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,16 +1,30 @@
 require 'pry'
 
 class InsertionSort
-attr_accessor :unsorted, :sorted
+# attr_accessor :unsorted, :sorted
 
-  def start(array)
-    @unsorted = array
-    @sorted = []
-    @sorted << @unsorted[0]
-    @unsorted.shift
+  # def start(array)
+  #   @unsorted = array
+  #   @sorted = []
+  #   @sorted << @unsorted[0]
+  #   @unsorted.shift
+  # end
+
+  def sort(array)
+    sorted = []
+    unsorted = array
+    sorted << unsorted.shift
+    until unsorted.empty?
+      if unsorted[0] < sorted[0]
+        sorted << unsorted.shift
+      elsif sorted.last == nil
+        sorted << unsorted[0]
+      else
+        sorted[i+1]
+        unsorted < sorted[i]
+        sorted << unsorted.shift
+      end
+    end
   end
-
-  def sort
-
 
 end
