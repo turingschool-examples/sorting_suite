21c74f1ef87faca8b1cccf7f37fd3d4c802f92de
Balmung421 <zcook421@gmail.com>
2017-07-24 00:12:21 -0600

    Added selection to benchmark; used same test on all to compare which is faster

--- benchmark.rb
+++ benchmark.rb
@@ -1,19 +1,22 @@
 require './bubble/lib/bubble'
 require './insertion/lib/insertion'
 require './merge/lib/merge'
+require './selection/lib/selection'
 require 'pry'
 
 
 time1 = Benchmark.measure do
   sorter = BubbleSort.new
   # p sorter.swaper(["d", "b", "a", "c"])
-  p sorter.swaper(('a'..'z').to_a.shuffle)
+  # p sorter.swaper(('a'..'z').to_a.shuffle)
+    p sorter.swaper((0..100).to_a.shuffle)
 end
 puts time1
 
 time2 = Benchmark.measure do
   sorter = InsertionSort.new
-  p sorter.insert(%w(b d f m z t r q o p w c e f j u l x n))
+  # p sorter.insert(%w(b d f m z t r q o p w c e f j u l x n))
+    p sorter.insert((0..100).to_a.shuffle)
 end
 puts time2
 
@@ -24,3 +27,11 @@ time3 = Benchmark.measure do
   p sorter.merge((0..100).to_a.shuffle)
 end
 puts time3
+
+time4 = Benchmark.measure do
+  sorter = SelectionSort.new
+  # p sorter.merge(%w(b d f m z t r q o p w c e f j u l x n))
+  # p sorter.merge(('a'..'z').to_a.shuffle)
+  p sorter.selection((0..100).to_a.shuffle)
+end
+puts time4
