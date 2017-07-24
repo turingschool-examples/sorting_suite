5fb4b6f45ffddf715879e4516243716fd41d5870
sidewinder2020 <sdwinder1@gmail.com>
2017-07-23 23:01:57 -0600

    Modified Bubble sort method entirely, using index and length now

---
+++ bubble/test/bubble_sort_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require 'pry'
+require './lib/bubble_sort'
+
+class BubbleSortTest < Minitest::Test
+
+  def test_it_exists
+    sorter = BubbleSort.new
+
+    assert_instance_of BubbleSort, sorter
+  end
+
+  def test_theres_an_array
+    skip
+    sorter = BubbleSort.new
+
+    assert_equals ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
+  end
+
+  def test_it_can_sort_numbers
+    sorter = BubbleSort.new
+
+    assert_equals [1, 2, 3, 4], sorter.sort[3, 1, 2, 4]
+  end
+
+end
