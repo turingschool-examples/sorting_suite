0b9ac869ef0aa03c72d4f3747b029edd6d4f7586
Balmung421 <zcook421@gmail.com>
2017-07-24 00:15:15 -0600

    Updated rake file to include selection test file

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,3 @@
 task :default do
-    FileList["./bubble/test/bubble_test.rb", "./insertion/test/insertion_test.rb", "./merge/test/merge_test.rb"].each { |file| ruby file }
+    FileList["./bubble/test/bubble_test.rb", "./insertion/test/insertion_test.rb", "./merge/test/merge_test.rb", "./selection/test/selection_test.rb"].each { |file| ruby file }
 end
