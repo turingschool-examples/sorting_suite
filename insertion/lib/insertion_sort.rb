1ccaca386860f65811cbcab3daf67bf67c5dcc78
sidewinder2020 <sdwinder1@gmail.com>
2017-07-23 20:22:02 -0600

    passed initial test in insertion sort

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,4 +1,16 @@
 require 'pry'
 
 class InsertionSort
+attr_accessor :unsorted, :sorted
+
+  def start(array)
+    @unsorted = array
+    @sorted = []
+    @sorted << @unsorted[0]
+    @unsorted.shift
+  end
+
+  def sort
+
+
 end
