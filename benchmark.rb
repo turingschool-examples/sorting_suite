242f21195edf2efbe1ac75f00617823c92b427a8
Balmung421 <zcook421@gmail.com>
2017-07-24 00:27:49 -0600

    Updated benchmark with colors to better read output

--- benchmark.rb
+++ benchmark.rb
@@ -2,23 +2,24 @@ require './bubble/lib/bubble'
 require './insertion/lib/insertion'
 require './merge/lib/merge'
 require './selection/lib/selection'
+require 'colorize'
 require 'pry'
-
-
+puts "##################################################".colorize(:blue)
+puts "Below are benchmarks for shuffled numbers for each sorting method in this project.".colorize(:color => :blue, :background => :white)
 time1 = Benchmark.measure do
   sorter = BubbleSort.new
   # p sorter.swaper(["d", "b", "a", "c"])
   # p sorter.swaper(('a'..'z').to_a.shuffle)
     p sorter.swaper((0..100).to_a.shuffle)
 end
-puts time1
+puts "Bubble Sort was this fast: #{time1}".colorize(:red)
 
 time2 = Benchmark.measure do
   sorter = InsertionSort.new
   # p sorter.insert(%w(b d f m z t r q o p w c e f j u l x n))
     p sorter.insert((0..100).to_a.shuffle)
 end
-puts time2
+puts "Insertion Sort was this fast: #{time2}".colorize(:red)
 
 time3 = Benchmark.measure do
   sorter = MergeSort.new
@@ -26,7 +27,7 @@ time3 = Benchmark.measure do
   # p sorter.merge(('a'..'z').to_a.shuffle)
   p sorter.merge((0..100).to_a.shuffle)
 end
-puts time3
+puts "Merge Sort was this fast: #{time3}".colorize(:red)
 
 time4 = Benchmark.measure do
   sorter = SelectionSort.new
@@ -34,4 +35,4 @@ time4 = Benchmark.measure do
   # p sorter.merge(('a'..'z').to_a.shuffle)
   p sorter.selection((0..100).to_a.shuffle)
 end
-puts time4
+puts "Selection Sort was this fast: #{time4}".colorize(:red)
