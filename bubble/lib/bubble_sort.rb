e6f9d97431d61c624645389d9daae26e76651854
Miguel Silva <msilva0897@gmail.com>
2017-09-09 11:47:08 -0600

    Bubble Sort finished

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,5 +1,17 @@
+require "pry"
 class BubbleSort
-  def sort(array, sorted_array = [])
-    
+
+  def sort(array)
+    count = 0
+    length = array.length - 1
+    length.times do |index|
+      if array[index] > array[index + 1]
+        array[index], array[index+1] = array[index+1], array[index]
+        count = 1
+      end
+    end
+    sort(array) if count == 1
+    array
   end
+
 end
