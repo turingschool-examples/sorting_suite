a833b78353ea08ac6e9ce7ce9afdb533e61ec2d3
Emily Wise <wise.m.emily@gmail.com>
2017-09-09 14:51:46 -0600

    removed comments

--- insertion/lib/insertion.rb
+++ insertion/lib/insertion.rb
@@ -24,25 +24,4 @@ class InsertionSort
     final
   end
 
-  # def sort(list)
-  #   length = list.length
-  #
-  #   sorted_list = list.shift
-  #   unsorted_list = list
-  #
-  #
-  # end
-
-#   function insertionSortR(array A, int n)
-#     if n>0
-#        insertionSortR(A,n-1)
-#        x ← A[n]
-#        j ← n-1
-#        while j >= 0 and A[j] > x
-#            A[j+1] ← A[j]
-#            j ← j-1
-#        end while
-#        A[j+1] ← x
-#     end if
-# end function
 end
