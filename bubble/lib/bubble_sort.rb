b88e75b706a99c6fed0175ca3aedb7a48480c84f
Emily Wise <wise.m.emily@gmail.com>
2017-09-09 14:04:36 -0600

    Complete Bubble

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,28 +1,23 @@
 require 'pry'
 class BubbleSort
 
+
   def sort(list)
     length = list.length
-    n = 1
-      while length != n
-        current_position = list[n]
-        previous_position = list[n-1]
-        if previous_position > current_position
-           current_position, previous_position = current_position, previous_position
-           n+= 1
-          
-        else
+    loop do
+    swapped = false
+
+        (length - 1).times do |n|
+          if list[n] > list[n+1]
+           list[n], list[n+1] = list[n+1], list[n]
+           swapped = true
+          end
         end
+
+      break if not swapped
       end
-
         list
-
-        # previous_position = array[index 1 - 1]
-        #evaluate if previous_position > than next position
-        # if true, swap current position and next
-        #and 1 to the index
-        #evaluate the next position
-        #then evaluate the next position
-    binding.pry
   end
+
+
 end
--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -9,17 +9,14 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort , sorter
   end
 
-  def test_swap_reverses_values
-      sorter = BubbleSort.new
-      expected = "b,a"
-
-
-      assert_equal expected, sorter.swap!(a,b)
-  end
-
   def test_sorts_strings_from_lowest_to_highest
     sorter = BubbleSort.new
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
+  end
+
+  def test_sorts_numbers
+    sorter = BubbleSort.new
+    assert_equal [2, 4, 4, 7, 8, 9, 10], sorter.sort([2,4,9,7,8,4,10])
   end
 
 end
