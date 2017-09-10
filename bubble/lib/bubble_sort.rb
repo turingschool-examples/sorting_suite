4b6adbce4d603b24ff2504ecdfa2550b527abd23
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-09 21:53:13 -0600

    add single_sort and sort methods

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,16 +1,29 @@
 class BubbleSort
 
-  def adv_current(current)
-    current += 1
-  end
-
-  def adv_previous(previous)
-    previous += 1
-  end
-
   def compare_values(previous, current, array)
-    if array[current] < array[previous]
+    if array[current].ord < array[previous].ord
       array[previous], array[current] = array[current], array[previous]
+    end
+    array
+  end
+
+  def single_sort(array)
+    length = array.length
+    previous = 0
+    current = 1
+    until current == length
+      compare_values(previous, current, array)
+      current += 1
+      previous += 1
+    end
+    array
+  end
+
+  def sort(array)
+    length = array.length
+    puts length
+    length.times do
+      single_sort(array)
     end
     array
   end
--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -11,20 +11,6 @@ class TestBubbleSort < Minitest::Test
     assert_instance_of BubbleSort, bubble_sort
   end
 
-  def test_advances_current
-    bubble_sort = BubbleSort.new
-    current = 0
-
-    assert_equal 1, bubble_sort.adv_current(current)
-  end
-
-  def test_advances_previous
-    bubble_sort = BubbleSort.new
-    previous = 0
-
-    assert_equal 1, bubble_sort.adv_previous(previous)
-  end
-
   def test_swaps_current_and_previous_if_c_is_less_than_p
     bubble_sort = BubbleSort.new
     current = 1
@@ -35,8 +21,32 @@ class TestBubbleSort < Minitest::Test
   end
 
   def test_does_not_swap_current_and_previous_if_c_is_greater_than_or_equal_to_p
-    skip
     bubble_sort = BubbleSort.new
+    current = 1
     previous = 0
+    array = [1, 2]
+
+    assert_equal [1, 2], bubble_sort.compare_values(previous, current, array)
   end
+
+  def test_bubbles_once_through_array
+    bubble_sort = BubbleSort.new
+    array = [3, 2, 1]
+
+    assert_equal [2, 1, 3], bubble_sort.single_sort(array)
+  end
+
+  def test_bubbles_though_array_length_number_of_times
+    bubble_sort = BubbleSort.new
+    array = [3, 2, 1]
+
+    assert_equal [1, 2, 3], bubble_sort.sort(array)
+  end
+
+  def test_sorts_array_of_letters
+    sorter = BubbleSort.new
+
+    assert_equal ['a', 'b', 'c', 'd'], sorter.sort(["d", "b", "a", "c"])
+  end
+
 end
