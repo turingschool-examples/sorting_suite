b0d220cd158422850876ac49064222184b844884
Dione Wilson <dionew1@gmail.com>
2017-09-10 13:12:27 -0600

    Add more tests to bubble_test

--- bubble/test/bubble_test.rb
+++ bubble/test/bubble_test.rb
@@ -10,10 +10,27 @@ class BubbleTest < Minitest::Test
     assert_instance_of BubbleSort, sorter
   end
 
+  def test_swap_letters
+    sorter = BubbleSort.new
+    actual = sorter.swap_letters(["d", "b", "a", "c"], "d", 0)
+    expected = ["b", "d"]
+
+    assert_equal expected, actual
+  end
+
+  def test_check_count
+    sorter = BubbleSort.new
+    actual = sorter.check_count(["b", "d", "a", "c"])
+    expected = ["a", "b", "c", "d"]
+
+    assert_equal expected, actual
+  end
+
   def test_sort
     sorter = BubbleSort.new
+    actual = sorter.sort(["d", "b", "a", "c"])
 
-    assert_equal ['a', 'b', 'c', 'd'], sorter.sort(["d", "b", "a", "c"])
+    assert_equal ['a', 'b', 'c', 'd'], actual
   end
 
 
