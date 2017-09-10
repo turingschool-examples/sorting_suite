bca5cd1c35b65d05df687e4db4142d191bcd65f5
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-09 22:03:06 -0600

    clean up tests

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -11,7 +11,7 @@ class TestBubbleSort < Minitest::Test
     assert_instance_of BubbleSort, bubble_sort
   end
 
-  def test_swaps_current_and_previous_if_c_is_less_than_p
+  def test_swaps_current_and_previous
     bubble_sort = BubbleSort.new
     current = 1
     previous = 0
@@ -20,7 +20,7 @@ class TestBubbleSort < Minitest::Test
     assert_equal [1, 2], bubble_sort.compare_values(previous, current, array)
   end
 
-  def test_does_not_swap_current_and_previous_if_c_is_greater_than_or_equal_to_p
+  def test_does_not_swap_current_and_previous
     bubble_sort = BubbleSort.new
     current = 1
     previous = 0
