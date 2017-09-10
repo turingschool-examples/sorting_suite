39298c733233e099cb58369ee4a026651c361580
Tim Tyrrell <timomitchel23@gmail.com>
2017-09-10 17:48:02 -0600

    test passing

--- merge/lib/merge_sort.rb
+++ merge/lib/merge_sort.rb
@@ -1,31 +1,35 @@
 class MergeSort
 
   def sort(array)
-    if array.length <=1
-      array
-    else
-      mid = (array.length / 2).floor
-      left_side = sort(array[0..mid- 1])
-      right_side = sort(array[mid..array.length])
-      merge(left_side, right_side)
+    def merge(left_sorted, right_sorted)
+      sorted = []
+      l = 0
+      r = 0
+
+      loop do
+        break if r >= right_sorted.length && l >= left_sorted.length
+
+        if r >= right_sorted.length || (l < left_sorted.length && left_sorted[l] < right_sorted[r])
+          sorted << left_sorted[l]
+          l += 1
+        else
+          sorted << right_sorted[r]
+          r += 1
+        end
+      end
+
+      return sorted
     end
 
-  end
+    def mergesort_iter(array_sliced)
+      return array_sliced if array_sliced.length <= 1
 
-  def merge(left, right)
-    if left.empty?
-      right
-    elsif right.empty?
-      left
-    elsif left.first > right.first
-      [left.first] + merge(left[1..left.length], right)
-    else
-      [right.first] + merge(left, right[1..right.length])
+      mid = array_sliced.length/2 - 1
+      left_sorted = mergesort_iter(array_sliced[0..mid])
+      right_sorted = mergesort_iter(array_sliced[mid+1..-1])
+      return merge(left_sorted, right_sorted)
     end
+
+    mergesort_iter(array)
   end
-
-
-
-
-
 end
