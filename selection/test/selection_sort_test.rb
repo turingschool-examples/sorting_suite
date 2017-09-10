1ff50bce0f04b8dfe0e6c83d7906ec05aabf4fd6
Josi <junipertmcclellan@gmail.com>
2017-09-09 22:55:06 -0600

    port tests into selection sort

---
+++ selection/test/selection_sort_test.rb
@@ -0,0 +1,74 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+
+require './merge/lib/merge_sort'
+
+
+class MergeSortTest < Minitest::Test
+
+  def test_insert_safe_sorts
+    original = [1,4,2,3,4,5,6,7,5,4,3,3,3,5,6,7,2]
+    assert_equal original.sort, MergeSort.new.sort(original)
+  end
+
+  def test_insert_bang_sorts
+    original = [1,4,2,3,4,5,6,7,5,4,3,3,3,5,6,7,2]
+    assert_equal original.sort, MergeSort.new.sort!(original)
+  end
+
+  def test_sort_safe_doesnt_change_the_source
+    original = [1,4,2,3,4,5,6,7,5,4,3,3,3,5,6,7,2]
+    argument = original.dup
+    MergeSort.new.sort argument
+    assert_equal original, argument
+  end
+
+  def test_sort_bang_sorts_in_place
+    original = [1,4,2,3,4,5,6,7,5,4,3,3,3,5,6,7,2]
+    sorted = MergeSort.new.sort!(original)
+    assert sorted.equal?(original)
+  end
+
+  def test_sort_safe_works_with_arbitrary_numbers
+    original = Array.new(1000){ rand(-100_000..100_000) }
+    assert_equal original.sort, MergeSort.new.sort(original)
+  end
+
+  def test_sort_bang_works_with_arbitrary_numbers
+    original = Array.new(1000){ rand(-100_000..100_000) }
+    assert_equal original.sort, MergeSort.new.sort!(original)
+  end
+
+  def test_sort_safe_works_with_strings
+    original = %W{
+      sharknado
+      hello
+      helLo
+      90063
+      90053
+      what's\ up?
+      forward\\slash
+      FoO!
+      @$%^&*
+      *&^%$@
+    }
+    assert_equal original.sort, MergeSort.new.sort(original)
+  end
+
+  def test_sort_bang_works_with_strings
+    original = %W{
+      sharknado
+      hello
+      helLo
+      90063
+      90053
+      what's\ up?
+      forward\\slash
+      FoO!
+      @$%^&*
+      *&^%$@
+    }
+    assert_equal original.sort, MergeSort.new.sort!(original)
+  end
+
+end
