1cab4f12f9d6b7ca377e4636151f2e03282971e6
Gabe Afflitto <gafflitto92@gmail.com>
2017-09-11 08:55:07 -0600

    final

--- insertion/lib/insertion.rb
+++ insertion/lib/insertion.rb
@@ -1,14 +1,17 @@
+require 'pry'
 class InsertionSort
     attr_reader :sorted
 
   def sort(unsorted, index = 0)
-    @sorted = []
-    sorted << unsorted[index]
-    index += 1
-    sorted << unsorted[index]
-    if sorted[index] > unsorted[index + 1]
-      sorted[index], sorted[index + 1] =  sorted[index + 1], index[index]
+    sorted = [sorted[index]]
+    unsorted.delete_at(index)
+    loop do
+      sorted << unsorted[index]
+      index += 1
+      sorted << unsorted[index]
+      if sorted[index] > unsorted[index + 1]
+        sorted[index], sorted[index + 1] =  sorted[index + 1], index[index]
+      end
     end
   end
-  
 end
