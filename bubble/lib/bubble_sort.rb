0617b676f814edb9f13a701a29ea5bb8216348f3
Hugh Morrison <hmorri32@gmail.com>
2017-09-10 18:23:29 -0600

    init

---
+++ bubble/lib/bubble_sort.rb
@@ -0,0 +1,14 @@
+class BubbleSort
+  def sort(array)
+    length = array.length 
+
+    for i in 0..length-1
+      for j in 0..n-i-1
+        if array[j] > array[j+1]
+          
+        end
+
+      end
+    end
+  end
+end
---
+++ bubble/test/bubble_sort_test.rb
@@ -0,0 +1,8 @@
+require_relative '../../test_helper.rb'
+
+class BubbleSortTest < Minitest::Test 
+
+  def test_this
+    assert true 
+  end
+end
