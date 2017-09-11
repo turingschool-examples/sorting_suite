7f3a0658d873f03c5aa63ccc8585fd714f2cddd1
katyjane8 <katy.jane8@gmail.com>
2017-09-11 08:30:01 -0600

    trying to merge sort

---
+++ merge/test/merge_sort_test.rb
@@ -0,0 +1,19 @@
+gem 'minitest'
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/merge_sort'
+
+class MergeSortTest < Minitest::Test
+  def test_sorter_exists
+    sorter = MergeSort.new
+
+    assert_instance_of MergeSort, sorter
+  end
+
+  def test_sorter_can_sort
+    sorter = MergeSort.new(["d", "b", "a", "c"])
+
+    assert_equal (["a", "b", "c", "d"]), sorter.sort
+  end
+
+end
