b858a1f1009911ce4aedb46d0800abfdfdce9f94
seoulstice <jungyoungjoon@gmail.com>
2017-09-11 07:37:00 -0600

    WIP

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,7 +2,14 @@ require 'pry'
 class Bubblesort
 
   def sort(array)
+    size = array.length
 
+    size.times do |number|
+      if array[number] > array[number + 1]
+        array[number], array[number + 1] = array[number + 1], array[number]
+      end
+    end
+    array
   end
 end
 
