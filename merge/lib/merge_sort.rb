2fb7f76663356ec5b25f56343a797413e1b4311a
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-10 19:20:07 -0600

    add sort_two method

--- merge/lib/merge_sort.rb
+++ merge/lib/merge_sort.rb
@@ -1,3 +1,11 @@
 class MergeSort
 
+  def sort_two(two_object_array)
+    if array[0] > array[1]
+      array[0], array[1] = array[1], array[0]
+    else
+      array
+    end
+  end
+
 end
--- merge/test/merge_sort_test.rb
+++ merge/test/merge_sort_test.rb
@@ -11,4 +11,11 @@ class TestMergeSort < Minitest::Test
     assert_instance_of MergeSort, merge_sort
   end
 
+  def test_sort_two_object_array
+    merge_sort = MergeSort.new
+
+    assert_equal [1, 2], merge_sort.sort_two([2, 1])
+    assert_equal [1, 2], merge_sort.sort_two([1, 2])
+  end
+
 end
