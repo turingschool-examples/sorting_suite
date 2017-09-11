4712b2203f5a7d19b2dc4889aced154f487e083f
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-10 21:30:02 -0600

    make selection_sort pass tests

--- selection/lib/selection_sort.rb
+++ selection/lib/selection_sort.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class SelectionSort
+
+  def lowest(unsorted, sorted)
+    lowest = unsorted[0]
+    unsorted.each.with_index do |value, i|
+      if unsorted[i + 1].nil?
+        nil
+      elsif value < unsorted[i + 1]
+        lowest = value
+      end
+    end
+    sorted << unsorted.delete(lowest)
+  end
+
+  def check_last(sorted)
+    if sorted[-1] < sorted[-2]
+      sorted[-2], sorted[-1] = sorted[-1], sorted[-2]
+    end
+  end
+
+  def selection_sort(unsorted)
+    sorted = []
+    until unsorted.empty?
+      lowest(unsorted, sorted)
+    end
+    check_last(sorted)
+    sorted
+  end
 
 end
--- selection/test/selection_sort_test.rb
+++ selection/test/selection_sort_test.rb
@@ -11,4 +11,16 @@ class TestSelectionSort < Minitest::Test
     assert_instance_of SelectionSort, selection
   end
 
+  def test_lowest_gets_lowest_value
+    selection = SelectionSort.new
+
+    assert_equal [1], selection.lowest([1, 2], [])
+  end
+
+  def test_selection_sort_sorts
+    selection = SelectionSort.new
+
+    assert_equal [1, 2, 3, 4], selection.selection_sort([4, 2, 1, 3])
+  end
+
 end
