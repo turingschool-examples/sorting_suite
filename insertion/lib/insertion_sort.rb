3a9bb0681ce1fb2ab9ece705ae0a1743492b5df5
Tyjoo26 <Tyjoo21@gmail.com>
2017-09-11 08:09:42 -0600

    Insertion sort tested and buble sort test

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -2,6 +2,15 @@ class InsertionSort
 
 
   def insertion_sort(list)
+    (1...list.length).each do |index|
+       position = index
+       while position > 0 && list[position] < list[position-1]
+         list[position], list[position-1] = list[position-1], list[position]
+         position -= 1
+       end
+   end
+   list
+ end
 
 
-end
+  end
--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -11,9 +11,15 @@ class InsertionSortTest < MiniTest::Test
   end
 
 
-  def insertion_sort
+  def test_insertion_sort_letters
     sorter = InsertionSort.new
 
-    assert_equal ["a", "b", "c", "d"], sorter.insertion_sort
+    assert_equal ["a", "b", "c", "d"], sorter.insertion_sort(["d","b","a","c"])
+  end
+
+  def test_insertion_sort_numbers
+    sorter = InsertionSort.new
+
+    assert_equal [1, 2, 3, 4, 5, 6, 7,8, 9, 10], sorter.insertion_sort([8,5,4,2,1,3,6,7,9,10])
   end
 end
