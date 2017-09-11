25d4d505352c2b4d6487a9b102e9e34d00353071
seoulstice <jungyoungjoon@gmail.com>
2017-09-11 07:23:30 -0600

    whoops

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,35 +2,7 @@ require 'pry'
 class Bubblesort
 
   def sort(array)
-    new_array = []
-    array.each_with_index do |letter, index|
 
-
-      if array[0] > array[1]
-        d = array[1]
-        b  = array[0]
-        new_array = [b, d]
-      end
-
-      a = array[2]
-      if d > a
-        new_array = [b, a, d]
-      end
-
-      c = array[3]
-      if d > c
-        new_array = [b, a, c, d]
-      end
-
-      if b > a
-        new_array = [a, b, c, d]
-      end
-
-
-
-
-
-    end
   end
 end
 
