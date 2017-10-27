9c6565dffbbcdcb9b4c195b62d178b2a7037a88f
abarnes26 <abarnes26@yahoo.com>
2017-10-27 15:33:35 -0600

    finished up to analyze loop

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -4,20 +4,22 @@
         => ["a", "b", "c", "d"]
 =end
 
+require 'pry'
+
 class Bubble
 
-  attr_reader :field
+  attr_reader :field, :previous, :current
 
   def sort(field)
     @field = field
     compare
   end
 
-  def previous
+  def find_previous
     @previous = @field[0]
   end
 
-  def current
+  def find_current
     @current = @field[1]
   end
 
@@ -25,11 +27,39 @@ class Bubble
     @previous <=> @current
   end
 
-  def change
-    compare
-    if compare == -1
-      @previous, @current = @current, @previous
+  def swap_value_prev
+    @field[@field.index(@previous)]
+  end
+
+  def swap_value_cur
+    @field[@field.index(@current)]
+  end
+
+  def swap
+    @field[@field.index(@previous)], @field[@field.index(@current)] = @field[@field.index(@current)], @field[@field.index(@previous)]
+  end
+
+  def analyze
+      compare
+      @field[(@field.index(@previous))..(@field.index(@current))] = swap[0..1] if compare == 1
+      move_next
+      @field
+  end
+
+  def analyze_loop
+    5.times do
+      analyze
     end
+    @field
+  end
+
+  def move_next
+      if field[field.index(@current)+1].nil?
+        @current = nil
+      else
+        @previous = field[field.index(@previous)+1]
+        @current = field[field.index(@current)+1]
+      end
   end
 
 end
