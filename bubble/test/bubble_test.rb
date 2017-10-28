9e0c228f0b4eb38f97bce9f1be4abd479af5c1d2
slimecog <wlcjohnson@gmail.com>
2017-10-28 12:33:59 -0600

    adds initial files

---
+++ bubble/test/bubble_test.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require "./bubble/lib/bubble"
+
+class BubbleTest < Minitest::Test
+end
---
+++ insertion/test/insertion_test.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require "./insertion/lib/insertion"
+
+class InsertionTest < Minitest::Test
+end
