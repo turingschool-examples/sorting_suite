ceeb93f0509c0998074f48463db4479d4e4874ba
slimecog <wlcjohnson@gmail.com>
2017-10-28 15:30:39 -0600

    syntax correction

--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -38,6 +38,6 @@ class BubbleTest < Minitest::Test
   def test_it_can_sort_multiple_digit_indices
     sorter = BubbleSort.new
 
-    assert_equal [0, 1, 12, 69, 353, 85832], sorter.sort([353,85832,0,12,1,69])
+    assert_equal [0, 1, 12, 69, 353, 85832], sorter.sort([353, 85832, 0, 12, 1, 69])
   end
 end
