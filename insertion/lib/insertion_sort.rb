8f5804a2764c5c9165cb221b82cdf752dc6e1329
maxscores <maxstackhouse@gmail.com>
2017-10-28 12:03:18 -0600

    Add instantiation

--- insertion/lib/insertion_sort.rb
+++ insertion/lib/insertion_sort.rb
@@ -1,26 +1,34 @@
 class InsertionSort
-  def self.sort(to_sort)
-    sorted = []
-    to_sort.count.times do
-      to_insert = to_sort.shift
-      if sorted.count == 0
-        sorted << to_insert
-      elsif
-        (sorted.count).times do |time|
-          if sorted[time] > to_insert
-            sorted.insert(time, to_insert)
-            break
-          elsif sorted[time] < to_insert && sorted[time+1].nil?
-            sorted.insert(time+1, to_insert)
-            break
-          elsif sorted[time] < to_insert && sorted[time+1] > to_insert
-            sorted.insert(time+1, to_insert)
-            break
-          end
-        end
-      end
+  attr_reader :to_sort, :sorted, :to_insert
+
+  def initialize(collection)
+    @to_sort = collection
+    @sorted = []
+    @to_insert = nil
+  end
+
+  def sort
+    (to_sort.count).times do
+      @to_insert = to_sort.shift
+      sorted << to_insert if sorted.count == 0
+      find_insert_location
     end
     sorted
+  end
+
+  def find_insert_location
+    (sorted.count).times do |time|
+      break sorted.insert(time, to_insert) if insert_is_less_than(time)
+      sorted.insert(time+1, to_insert) if insert_is_greater_than_and_last(time)
+    end
+  end
+
+  def insert_is_less_than(time)
+    sorted[time] > to_insert
+  end
+
+  def insert_is_greater_than_and_last(time)
+    sorted[time] < to_insert && sorted[time+1].nil?
   end
 
 end
--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -5,21 +5,24 @@ require './lib/insertion_sort'
 class TestInsertionSort < Minitest::Test
   def test_can_order_three_values
     numbers = [0, 2, 1]
+    insertion_sort = InsertionSort.new(numbers)
 
-    assert_equal [0, 1, 2], InsertionSort.sort(numbers)
+    assert_equal [0, 1, 2], insertion_sort.sort
   end
 
   def test_can_order_5_values
     numbers = [0, 23, 1, 4, 64]
+    insertion_sort = InsertionSort.new(numbers)
 
-    assert_equal [0, 1, 4, 23, 64], InsertionSort.sort(numbers)
+    assert_equal [0, 1, 4, 23, 64], insertion_sort.sort
   end
 
   def test_can_order_7_values
     numbers = [24, 53, 432, 2, 8, 69, 37]
+    insertion_sort = InsertionSort.new(numbers)
     sorted = numbers.sort
 
-    assert_equal sorted, InsertionSort.sort(numbers)
+    assert_equal sorted, insertion_sort.sort
   end
 
   def test_range_is_valid
@@ -31,15 +34,17 @@ class TestInsertionSort < Minitest::Test
 
   def test_can_order_100_values
     numbers = (0..100).map {|num| num}.shuffle
+    insertion_sort = InsertionSort.new(numbers)
     sorted = (0..100).map {|num| num}
 
-    assert_equal sorted, InsertionSort.sort(numbers)
+    assert_equal sorted, insertion_sort.sort
   end
 
   def test_can_order_10000_values
     numbers = (0..10000).map {|num| num}.shuffle
+    insertion_sort = InsertionSort.new(numbers)
     sorted = (0..10000).map {|num| num}
 
-    assert_equal sorted, InsertionSort.sort(numbers)
+    assert_equal sorted, insertion_sort.sort
   end
 end
