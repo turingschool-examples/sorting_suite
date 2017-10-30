f85dba8ea5599bb7a9ff9d8a255a6cb26a5bfcf8
abarnes26 <abarnes26@yahoo.com>
2017-10-29 20:23:33 -0600

    completely stripped down and redid. Functional, lacking tests

--- bubble/lib/bubble.rb
+++ bubble/lib/bubble.rb
@@ -4,62 +4,29 @@
         => ["a", "b", "c", "d"]
 =end
 
-require 'pry'
-
-class Bubble
-
-  attr_reader :field, :previous, :current
+class BubbleSort
 
   def sort(field)
-    @field = field
-    compare
-  end
-
-  def find_previous
-    @previous = @field[0]
-  end
-
-  def find_current
-    @current = @field[1]
-  end
-
-  def compare
-    @previous <=> @current
-  end
-
-  def swap_value_prev
-    @field[@field.index(@previous)]
-  end
-
-  def swap_value_cur
-    @field[@field.index(@current)]
-  end
-
-  def swap
-    @field[@field.index(@previous)], @field[@field.index(@current)] = @field[@field.index(@current)], @field[@field.index(@previous)]
-  end
-
-  def analyze
-      compare
-      @field[(@field.index(@previous))..(@field.index(@current))] = swap[0..1] if compare == 1
-      move_next
-      @field
-  end
-
-  def analyze_loop
-    5.times do
-      analyze
+    (field.count).times do
+      num1 = 0
+      num2 = 1
+      compare_and_replace_loop(num1, num2, field)
     end
-    @field
+    puts field
   end
 
-  def move_next
-      if field[field.index(@current)+1].nil?
-        @current = nil
-      else
-        @previous = field[field.index(@previous)+1]
-        @current = field[field.index(@current)+1]
-      end
+ def compare_and_replace(num1, num2, field)
+  if (field[num1] <=> field[num2]) == 1
+  field[num1], field[num2] = field[num2], field[num1]
+  field
   end
+ end
 
+ def compare_and_replace_loop(num1, num2, field)
+   (field.count).times do
+     compare_and_replace(num1,num2, field)
+     num1 += 1
+     num2 += 1
+   end
+ end
 end
