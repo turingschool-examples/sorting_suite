7d1feee2c5df954b919d80233aabd0546216b91e
maxscores <maxstackhouse@gmail.com>
2017-10-30 07:59:52 -0600

    Add benchmarker

--- insertion/test/insertion_sort_test.rb
+++ insertion/test/insertion_sort_test.rb
@@ -5,24 +5,24 @@ require './lib/insertion_sort'
 class TestInsertionSort < Minitest::Test
   def test_can_order_three_values
     numbers = [0, 2, 1]
-    insertion_sort = InsertionSort.new(numbers)
+    insertion_sort = InsertionSort.new
 
-    assert_equal [0, 1, 2], insertion_sort.sort
+    assert_equal [0, 1, 2], insertion_sort.sort(numbers)
   end
 
   def test_can_order_5_values
     numbers = [0, 23, 1, 4, 64]
-    insertion_sort = InsertionSort.new(numbers)
+    insertion_sort = InsertionSort.new
 
-    assert_equal [0, 1, 4, 23, 64], insertion_sort.sort
+    assert_equal [0, 1, 4, 23, 64], insertion_sort.sort(numbers)
   end
 
   def test_can_order_7_values
     numbers = [24, 53, 432, 2, 8, 69, 37]
-    insertion_sort = InsertionSort.new(numbers)
+    insertion_sort = InsertionSort.new
     sorted = numbers.sort
 
-    assert_equal sorted, insertion_sort.sort
+    assert_equal sorted, insertion_sort.sort(numbers)
   end
 
   def test_range_is_valid
@@ -34,17 +34,17 @@ class TestInsertionSort < Minitest::Test
 
   def test_can_order_100_values
     numbers = (0..100).map {|num| num}.shuffle
-    insertion_sort = InsertionSort.new(numbers)
+    insertion_sort = InsertionSort.new
     sorted = (0..100).map {|num| num}
 
-    assert_equal sorted, insertion_sort.sort
+    assert_equal sorted, insertion_sort.sort(numbers)
   end
 
   def test_can_order_10000_values
     numbers = (0..10000).map {|num| num}.shuffle
-    insertion_sort = InsertionSort.new(numbers)
+    insertion_sort = InsertionSort.new
     sorted = (0..10000).map {|num| num}
 
-    assert_equal sorted, insertion_sort.sort
+    assert_equal sorted, insertion_sort.sort(numbers)
   end
 end
--- merge/lib/merge_sort.rb
+++ merge/lib/merge_sort.rb
@@ -1,5 +1,3 @@
-require './lib/insertion_sort'
-
 class MergeSort
   attr_reader :sorted
 
