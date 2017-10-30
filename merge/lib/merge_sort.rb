ada9beaccae9a858544b469b2429af3c0da4e97c
maxscores <maxstackhouse@gmail.com>
2017-10-30 08:11:00 -0600

    Refactor

--- merge/lib/merge_sort.rb
+++ merge/lib/merge_sort.rb
@@ -5,18 +5,26 @@ class MergeSort
     @sorted = []
   end
 
-  def sort(elements)
-    size = elements.count
-    return elements if size <= 1
-    half_1 = []
-    half_2 = []
+  def sort(elements, half_1 = [], half_2 = [])
+    return elements if elements.count <= 1
+    divide_collection(elements, half_1, half_2)
+    sort_singles(half_1, half_2)
+    half_1 = sort(half_1.flatten)
+    half_2 = sort(half_2.flatten)
+    return merge(half_1.flatten, half_2.flatten)
+  end
+
+  def divide_collection(elements, half_1, half_2)
     elements.each.with_index do |element, index|
-      if index < size/2
+      if index < elements.count/2
         half_1 << element
       else
         half_2 << element
       end
     end
+  end
+
+  def sort_singles(half_1, half_2)
     if half_1.length == 1 && half_2.length == 1
       if half_2.first < half_1.first
         return half_2, half_1
@@ -24,13 +32,16 @@ class MergeSort
         return half_1, half_2
       end
     end
-    half_1 = sort(half_1.flatten)
-    half_2 = sort(half_2.flatten)
-    return merge(half_1.flatten, half_2.flatten)
   end
 
-  def merge(half_1, half_2)
-    result = []
+  def merge(half_1, half_2, result = [])
+    merge_until_one_half_empty(half_1, half_2, result)
+    result << half_1.shift while !half_1.empty?
+    result << half_2.shift while !half_2.empty?
+    result
+  end
+
+  def merge_until_one_half_empty(half_1, half_2, result)
     while !half_1.empty? && !half_2.empty?
       if half_1.first <= half_2.first
         result << half_1.shift
@@ -38,13 +49,6 @@ class MergeSort
         result << half_2.shift
       end
     end
-    while !half_1.empty?
-      result << half_1.shift
-    end
-    while !half_2.empty?
-      result << half_2.shift
-    end
-    result
   end
 
 end
