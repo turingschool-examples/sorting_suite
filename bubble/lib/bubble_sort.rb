89ecec1d8a597f4842f5185c674d8f5d7a2cbe77
Anna Royer <anna.royer@colorado.edu>
2017-12-26 12:03:40 -0700

    add first cycle bubble sorting, needs to continue until all are sorted

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,15 +1,17 @@
+require 'pry'
 class BubbleSort
-  def initialize
-    @previous = nil
-    @current = nil
-  end
 
   def sort(collection)
-    @previous = collection[0]
-    @current = collection[1]
-  end
-
-  def swap?
-    @previous > @current
+    collection.each_with_index do |element, index|
+      binding.pry
+      previous = element
+      current = collection[index + 1]
+      if previous > current
+        collection[index] = current
+        collection[index + 1] = previous
+      end
+    end
+    collection
   end
 end
+binding.pry
--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -4,16 +4,8 @@ require './lib/bubble_sort'
 
 class BubbleSortTest < Minitest::Test
   def test_it_can_sort_a_collection
-    skip
     sorter = BubbleSort.new
 
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
-  end
-
-  def test_it_decides_whether_to_swap_two_elements
-    sorter = BubbleSort.new
-    sorter.sort(["a", "b", "c", "d"])
-
-    refute sorter.swap?
   end
 end
