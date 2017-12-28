16a28ee20da736d595d2692a645853fcd9436bdb
Luke Chambers <lukenatchambers@gmail.com>
2017-12-28 11:12:39 -0700

    Full functionality

--- merge/lib/merge_sort.rb
+++ merge/lib/merge_sort.rb
@@ -1,10 +1,6 @@
 require "pry"
 
 class MergeSort
-  
-  def initialize
-    @result = []
-  end
 
   def sort(to_sort)
     puts pre_sort(to_sort)
@@ -14,24 +10,28 @@ class MergeSort
   end
 
   def slice_arrays(to_sort)
-    if to_sort.length <= 1
-      to_sort
+    if to_sort.length == 1
+      return to_sort
     else
       middle = (to_sort.length / 2)
       left = to_sort[0..middle - 1]
       right = to_sort[middle..-1]
     end
-    merge(left, right)
+    to_sort_left = slice_arrays(left)
+    to_sort_right = slice_arrays(right)
+    merge(to_sort_left, to_sort_right)
   end
 
   def merge(left, right)
-    if left[0].nil?
-      right
-    elsif left.first > right.first
-      [left.first] + merge(left[1..left.length], right)
-    else
-      [right.first] + merge(left, right[1..right.length])
+    result = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        result << left.shift
+      else
+        result << right.shift
+      end
     end
+    return result.concat(left,right)
   end
 
   def pre_sort(to_sort)
