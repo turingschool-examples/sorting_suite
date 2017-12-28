1b593e6d049f046be385e6890b7e292d4c64a3a8
Luke Chambers <lukenatchambers@gmail.com>
2017-12-28 15:05:23 -0700

    Build framework for insertion_sort

---
+++ insertion/lib/insertion_sort.rb
@@ -0,0 +1,32 @@
+require "pry"
+
+class InsertionSort
+
+  def initialize
+    @sorted = []
+  end
+
+  def sort(to_sort)
+    to_sort = start_format(to_sort)
+
+  def start_format(to_sort)
+    to_sort.reduce([]) do |result, letter|
+      result << letter.ord
+    end
+  end
+
+  def finish_format(to_sort)
+    to_sort.reduce([]) do |result, number|
+      result << number.chr
+    end
+  end
+
+  def pre_sort(to_sort)
+    if to_sort[0].class == Integer || to_sort[0].class == String
+      "Sorting..."
+    else
+      "Please put in letters or numbers."
+    end
+  end
+
+end
