bff73d349115f18477352490c9230ad05cf89d3a
Lihkin <shniks@gmail.com>
2017-12-30 15:13:45 -0700

    Cleaned up the sort method.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,32 +2,21 @@ require 'pry'
 
 class BubbleSort
 
-  def asks_user_for_an_input
-    print "Please enter alphabets or a number: "
-    @user_input = gets.chomp.split("")
-    @length = @user_input.length
-  end
-
-  def sort
-    asks_user_for_an_input
+  def sort(user_input)
     number = 0
-    while number < (@length - 1)
-      count = 0
-      while count < (@length - 1)
-        previous = @user_input[count]
-        current = @user_input[count + 1]
-        if current < previous
-          @user_input[count], @user_input[count + 1] = @user_input[count + 1], @user_input[count]
-        else
-          @user_input[count], @user_input[count + 1] = @user_input[count], @user_input[count + 1]
+    while number < (user_input.length - 1)
+      user_input.each_with_index do |value, index|
+        if user_input[index + 1].nil?
+          value
+        elsif value > user_input[index + 1]
+          user_input[index], user_input[index + 1] = user_input[index + 1], user_input[index]
         end
-        count += 1
       end
       number += 1
     end
-    binding.pry
+    print user_input
   end
 
 end
 
-BubbleSort.new.sort
+BubbleSort.new.sort(["z", "k", "l", "p", "t", "r", "a"])
