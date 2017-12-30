e671c09fc0caf9e088ad0c99fb18b712ccd4206a
Lihkin <shniks@gmail.com>
2017-12-30 15:53:17 -0700

    Refactored bubble_sort and added additonal tests.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -2,9 +2,7 @@ require 'pry'
 
 class BubbleSort
 
-  def sort(user_input)
-    number = 0
-    while number < (user_input.length - 1)
+  def sort_one_pass(user_input)
       user_input.each_with_index do |value, index|
         if user_input[index + 1].nil?
           value
@@ -12,11 +10,13 @@ class BubbleSort
           user_input[index], user_input[index + 1] = user_input[index + 1], user_input[index]
         end
       end
-      number += 1
+  end
+
+  def sort(user_input)
+    user_input.length.times do
+      sort_one_pass(user_input)
     end
-    print user_input
+    user_input
   end
 
 end
-
-BubbleSort.new.sort(["z", "k", "l", "p", "t", "r", "a"])
--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -12,9 +12,32 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort, sorter
   end
 
-  
+  def test_sort_one_pass_with_alphabets
+    sorter = BubbleSort.new
+    result = sorter.sort_one_pass(["z", "k", "l", "p", "t", "r", "a"])
 
+    assert_equal ["k", "l", "p", "t", "r", "a", "z"], result
+  end
 
+  def test_sort_one_pass_with_numbers
+    sorter = BubbleSort.new
+    result = sorter.sort_one_pass([7, 9, 1, 55, 22, 14, 99, 3, 8])
 
+    assert_equal [7, 1, 9, 22, 14, 55, 3, 8, 99], result
+  end
+
+  def test_sort_with_alphabets
+    sorter = BubbleSort.new
+    result = sorter.sort(["z", "k", "l", "p", "t", "r", "a"])
+
+    assert_equal ["a", "k", "l", "p", "r", "t", "z"], result
+  end
+
+  def test_sort_with_numbers
+    sorter = BubbleSort.new
+    result = sorter.sort([7, 9, 1, 55, 22, 14, 99, 3, 8])
+
+    assert_equal [1, 3, 7, 8, 9, 14, 22, 55, 99], result
+  end
 
 end
