c5cfe09768899e9229d5ec1b8f6637bb014dcc10
Lihkin <shniks@gmail.com>
2017-12-30 13:51:03 -0700

    Changed sort method.

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,14 +1,23 @@
 require 'pry'
+
 class BubbleSort
 
   def asks_user_for_an_input
     print "Please enter alphabets or a number: "
-    user_input = gets.chomp.split("")
+    @user_input = gets.chomp.split("")
+    @length = @user_input.length
   end
 
-
-
-
-
+  def sort
+    asks_user_for_an_input
+    count = 0
+    while count < @length
+      previous = @user_input[count]
+      current = @user_input[count + 1]
+      count += 1
+    end
+  end
 
 end
+
+BubbleSort.new.sort
